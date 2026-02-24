228df7bb8f108fe17855b9e50113ad9eea24d85c
Samuel04-png <skamanga85@gmail.com>
2026-02-24 23:15:52 +0200

    feat: configure custom domain kimbalert.byteandberry.com

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/Kimbalert-africa">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
